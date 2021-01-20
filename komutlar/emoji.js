1a3048a21069bfec3855058fc65c8aecfa482dd7
mmelihm <77734372+mmelihm@users.noreply.github.com>
2021-01-20 16:47:34 +0300

    Create emoji.js

---
+++ komutlar/emoji.js
@@ -0,0 +1,355 @@
+const Discord = require('discord.js')
+
+exports.run = async (client, message, args) => {
+  
+  const db = require('quick.db');
+  const emoji = args.slice(0).join(' ');
+
+  if (emoji === "palyaco") {
+    message.delete();
+    message.channel.send(`<a:palyaco:700313810904547331>`)};
+  if (emoji === "opucuk") {
+    message.delete();
+    message.channel.send(`<a:opucuk:700313818965868585>`)};
+  if (emoji === "onay") {
+    message.delete();
+    message.channel.send(`<a:onay:700313803300143104>`)};
+  if (emoji === "nono") {
+    message.delete();
+    message.channel.send(`<a:nono:700313815086137435>`)};
+  if (emoji === "noktaloading") {
+    message.delete();
+    message.channel.send(`<a:noktaloading:700313810749358143>`)};
+  if (emoji === "no") {
+    message.delete();
+    message.channel.send(`<a:no:700313819968438292>`)};
+  if (emoji === "nitro") {
+    message.delete();
+    message.channel.send(`<a:nitro:700313813999812669>`)};
+  if (emoji === "nana") {
+    message.delete();
+    message.channel.send(`<a:nana:700313832249360456>`)};
+  if (emoji === "meow") {
+    message.delete();
+    message.channel.send(`<a:meow:700313815312629820>`)};
+  if (emoji === "lvlup") {
+    message.delete();
+    message.channel.send(`<a:lvlup:700313818395312228>`)};
+  if (emoji === "loading") {
+    message.delete();
+    message.channel.send(`<a:loading:700313814603923466>`)};
+  if (emoji === "kustum") {
+    message.delete();
+    message.channel.send(`<a:kustum:700313809050665010>`)};
+  if (emoji === "kurtcuk") {
+    message.delete();
+    message.channel.send(`<a:kurtcuk:700313831540523009>`)};
+  if (emoji === "kuduruk") {
+    message.delete();
+    message.channel.send(`<a:kuduruk:700313812359839777>`)};
+  if (emoji === "kudur") {
+    message.delete();
+    message.channel.send(`<a:kudur:700313808349954068>`)};
+  if (emoji === "kucuktur") {
+    message.delete();
+    message.channel.send(`<a:kucuktur:700313805925908541>`)};
+  if (emoji === "kopus") {
+    message.delete();
+    message.channel.send(`<a:kopus:700313838318518373>`)};
+  if (emoji === "kitap") {
+    message.delete();
+    message.channel.send(`<a:kitap:700313803304468480>`)};
+  if (emoji === "kelebek") {
+    message.delete();
+    message.channel.send(`<a:kelebek:700313836615630899>`)};
+  if (emoji === "kedicik") {
+    message.delete();
+    message.channel.send(`<a:kedicik:700313833775824988>`)};
+  if (emoji === "kalps") {
+    message.delete();
+    message.channel.send(`<a:kalps:700313802985570355>`)};
+  if (emoji === "kalpp") {
+    message.delete();
+    message.channel.send(`<a:kalpp:700313804466159658>`)};
+  if (emoji === "kalpm") {
+    message.delete();
+    message.channel.send(`<a:kalpm:700313803123982346>`)};
+  if (emoji === "kalpb") {
+    message.delete();
+    message.channel.send(`<a:kalpb:700313810053103727>`)};
+  if (emoji === "kalp") {
+    message.delete();
+    message.channel.send(`<a:kalp:700313801060253747>`)};
+  if (emoji === "isin") {
+    message.delete();
+    message.channel.send(`<a:isin:700313806722695188>`)};
+  if (emoji === "hypesquad") {
+    message.delete();
+    message.channel.send(`<a:hypesquad:700313802545037372>`)};
+  if (emoji === "hoj") {
+    message.delete();
+    message.channel.send(`<a:hoj:700313832924381277>`)};
+  if (emoji === "hmm") {
+    message.delete();
+    message.channel.send(`<a:hmm:700313810380259360>`)};
+  if (emoji === "ez") {
+    message.delete();
+    message.channel.send(`<a:ez:700313803862179861>`)};
+  if (emoji === "elmas") {
+    message.delete();
+    message.channel.send(`<a:elmas:700313831141802104>`)};
+  if (emoji === "dng") {
+    message.delete();
+    message.channel.send(`<a:dng:700313821268410368>`)};
+  if (emoji === "dikkat") {
+    message.delete();
+    message.channel.send(`<a:dikkat:700313802293379112>`)};
+  if (emoji === "darklord") {
+    message.delete();
+    message.channel.send(`<a:darklord:700313827153018941>`)};
+  if (emoji === "kedi") {
+    message.delete();
+    message.channel.send(`<a:kedi:700313823697043467>`)};
+  if (emoji === "carpi") {
+    message.delete();
+    message.channel.send(`<a:carpi:700313805128728576>`)};
+  if (emoji === "classic") {
+    message.delete();
+    message.channel.send(`<a:classic:700313831825604659>`)};
+  if (emoji === "buyuktur") {
+    message.delete();
+    message.channel.send(`<a:buyuktur:700313805351288844>`)};
+  if (emoji === "bune") {
+    message.delete();
+    message.channel.send(`<a:bune:700313811940409397>`)};
+  if (emoji === "buki") {
+    message.delete();
+    message.channel.send(`<a:buki:700313825072906261>`)};
+  if (emoji === "boost") {
+    message.delete();
+    message.channel.send(`<a:boost:700313818902822974>`)};
+  if (emoji === "bicak") {
+    message.delete();
+    message.channel.send(`<a:bicak:700313804868681768>`)};
+  if (emoji === "bebis") {
+    message.delete();
+    message.channel.send(`<a:bebis:700313838289158184>`)};
+  if (emoji === "battaniye") {
+    message.delete();
+    message.channel.send(`<a:battaniye:700313823529140325>`)};
+  if (emoji === "banned") {
+    message.delete();
+    message.channel.send(`<a:banned:700313817799721050>`)};
+  if (emoji === "alkis") {
+    message.delete();
+    message.channel.send(`<a:alkis:700313814674964571>`)};
+  if (emoji === "alev") {
+    message.delete();
+    message.channel.send(`<a:alev:700313806064320522>`)};
+  if (emoji === "agabe") {
+    message.delete();
+    message.channel.send(`<a:agabe:700313803144953908>`)};
+  if (emoji === "aaa") {
+    message.delete();
+    message.channel.send(`<a:aaa:700313823873335307>`)};
+  if (emoji === "200iq") {
+    message.delete();
+    message.channel.send(`<a:200iq:700313824275988572>`)};
+  if (emoji === "zipzip") {
+    message.delete();
+    message.channel.send(`<a:zipzip:700314096502833172>`)};
+  if (emoji === "yldzm") {
+    message.delete();
+    message.channel.send(`<a:yldzm:700314124181176320>`)};
+  if (emoji === "yldzk") {
+    message.delete();
+    message.channel.send(`<a:yldzk:700314124361400330>`)};
+  if (emoji === "yildiz") {
+    message.delete();
+    message.channel.send(`<a:yildiz:700314095013986372>`)};
+  if (emoji === "yes") {
+    message.delete();
+    message.channel.send(`<a:yes:700314099212484639>`)};
+  if (emoji === "yeap") {
+    message.delete();
+    message.channel.send(`<a:yeap:700314111552258068>`)};
+  if (emoji === "yaniyorum") {
+    message.delete();
+    message.channel.send(`<a:yaniyorum:700314121983492097>`)};
+  if (emoji === "yalarim2") {
+    message.delete();
+    message.channel.send(`<a:yalarim2:700314119403733102>`)};
+  if (emoji === "yalarim1") {
+    message.delete();
+    message.channel.send(`<a:yalarim1:700314116258267167>`)};
+  if (emoji === "yalarim") {
+    message.delete();
+    message.channel.send(`<a:yalarim:700314109920673843>`)};
+  if (emoji === "viuw") {
+    message.delete();
+    message.channel.send(`<a:viuw:700314094967980053>`)};
+  if (emoji === "viuv") {
+    message.delete();
+    message.channel.send(`<a:viuv:700314091490639963>`)};
+  if (emoji === "tiky") {
+    message.delete();
+    message.channel.send(`<a:tiky:700314124948602972>`)};
+  if (emoji === "tiks") {
+    message.delete();
+    message.channel.send(`<a:tiks:700314109220225035>`)};
+  if (emoji === "tikp") {
+    message.delete();
+    message.channel.send(`<a:tikp:700314120880259082>`)};
+  if (emoji === "tikm") {
+    message.delete();
+    message.channel.send(`<a:tikm:700314125452181544>`)};
+  if (emoji === "smiler") {
+    message.delete();
+    message.channel.send(`<a:smiler:700314105617317938>`)};
+  if (emoji === "serefe") {
+    message.delete();
+    message.channel.send(`<a:serefe:700314101200715826>`)};
+  if (emoji === "sad") {
+    message.delete();
+    message.channel.send(`<a:sad:700314098692259850>`)};
+  if (emoji === "pug") {
+    message.delete();
+    message.channel.send(`<a:pug:700314116853858446>`)};
+  if (emoji === "pepeg") {
+    message.delete();
+    message.channel.send(`<a:pepeg:700314094221131896>`)};
+  if (emoji === "patkut") {
+    message.delete();
+    message.channel.send(`<a:patkut:700314124885819422>`)};
+  if (emoji === "parmak") {
+    message.delete();
+    message.channel.send(`<a:parmak:700314089120989255>`)};
+  if (emoji === "melik") {
+    message.delete();
+    message.channel.send(`<a:aaa:700313823873335307><a:aaa:700313823873335307><a:aaa:700313823873335307><a:aaa:700313823873335307><a:aaa:700313823873335307>`)};
+  if (emoji === "dokuz") {
+    message.delete();
+    message.channel.send(`<a:dokuz:702495159539204176>`)};
+  if (emoji === "sekiz") {
+    message.delete();
+    message.channel.send(`<a:sekiz:702495159887331358>`)};
+  if (emoji === "yedi") {
+    message.delete();
+    message.channel.send(`<a:yedi:702495159287545936>`)};
+  if (emoji === "alti") {
+    message.delete();
+    message.channel.send(`<a:alti:702495159480221746>`)};
+  if (emoji === "bes") {
+    message.delete();
+    message.channel.send(`<a:bes:702495159123968052>`)};
+  if (emoji === "dort") {
+    message.delete();
+    message.channel.send(`<a:dort:702495159102865499>`)};
+  if (emoji === "uc") {
+    message.delete();
+    message.channel.send(`<a:uc:702495159530815488>`)};
+  if (emoji === "iki") {
+    message.delete();
+    message.channel.send(`<a:iki:702495159354392677>`)};
+  if (emoji === "bir") {
+    message.delete();
+    message.channel.send(`<a:bir:702495158867853387>`)};
+  if (emoji === "sifir") {
+    message.delete();
+    message.channel.send(`<a:sifir:702495154753241140>`)};
+  if (emoji === "uzulme") {
+    message.delete();
+    message.channel.send(`<a:uzulme:702496307566084107>`)};
+  if (emoji === "ucuyorum") {
+    message.delete();
+    message.channel.send(`<a:ucuyorum:702496306169380994>`)};
+  if (emoji === "tivsan") {
+    message.delete();
+    message.channel.send(`<a:tivsan:702496307667009566>`)};
+  if (emoji === "sdnz") {
+    message.delete();
+    message.channel.send(`<a:sdnz:702496305498292315>`)};
+  if (emoji === "planet") {
+    message.delete();
+    message.channel.send(`<a:planet:702496305322393610>`)};
+  if (emoji === "olala") {
+    message.delete();
+    message.channel.send(`<a:olala:702496309264777286>`)};
+  if (emoji === "nee") {
+    message.delete();
+    message.channel.send(`<a:nee:702496304995237958>`)};
+  if (emoji === "nanana") {
+    message.delete();
+    message.channel.send(`<a:nanana:702496357553799291>`)};
+  if (emoji === "mrrr") {
+    message.delete();
+    message.channel.send(`<a:mrrr:702496357293883432>`)};
+  if (emoji === "miyav") {
+    message.delete();
+    message.channel.send(`<a:miyav:702496358199853168>`)};
+  if (emoji === "mable") {
+    message.delete();
+    message.channel.send(`<a:mable:702496304554573884>`)};
+  if (emoji === "dipper") {
+    message.delete();
+    message.channel.send(`<a:dipper:702496303736946709>`)};
+  if (emoji === "kalpi") {
+    message.delete();
+    message.channel.send(`<a:kalpi:702496304298852442>`)};
+  if (emoji === "hagh") {
+    message.delete();
+    message.channel.send(`<a:hagh:702496307121487892>`)};
+  if (emoji === "cuut") {
+    message.delete();
+    message.channel.send(`<a:cuut:702496304193863721>`)};
+  if (emoji === "cikcik") {
+    message.delete();
+    message.channel.send(`<a:cikcik:702496303183298604>`)};
+  if (emoji === "bambam") {
+    message.delete();
+    message.channel.send(`<a:bambam:702496304357441628>`)};
+  if (emoji === "pies") {
+    message.delete();
+    message.channel.send(`<a:pies:702496802452013136>`)};
+  if (emoji === "ivj") {
+    message.delete();
+    message.channel.send(`<a:ivj:702496800506118195>`)};
+  if (emoji === "gul") {
+    message.delete();
+    message.channel.send(`<a:gul:702496802968174642>`)};
+  if (emoji === "opucuks") {
+    message.delete();
+    message.channel.send(`<a:opucuks:703253348618338314>`)};
+  if (emoji === "yikik") {
+    message.delete();
+    message.channel.send(`<a:yikik:703253348777852989>`)};
+  if (emoji === "hh") {
+    message.delete();
+    message.channel.send(`<a:hh:703253349084037180>`)};
+  if (emoji === "crn") {
+    message.delete();
+    message.channel.send(`<a:crn:703253348299702364>`)};
+  if (emoji === "amag") {
+    message.delete();
+    message.channel.send(`<a:amag:703253349704663051>`)};
+  
+  
+
+      
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ["emoji-bul", "emojiara","emojibul","emoji-ara"],
+    permLevel: 0,
+    kategori: "kullanıcı",
+ 
+  };
+
+  exports.help = {
+    name: 'emoji',
+    description: 'İstediğiniz bir emojiyi botun ekli olduğu sunucularda aramanıza yarar.',
+    usage: 'emoji-ara <emoji-adı>',
+   
+  };
